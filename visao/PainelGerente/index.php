380822b3328ead58c4c6a08ed1bf55077f9b72f0
Mateus Henrique <112947808+mateushen@users.noreply.github.com>
2022-11-03 22:35:53 -0300

    Painel de gerente

--- visao/PainelGerente/index.php
+++ visao/PainelGerente/index.php
@@ -17,13 +17,57 @@
             <img src="../img/title.png" />
         </div>
         <div class="img-exit">
-            <a href="../../main.php"><img src="../img/icon-exit.png" width="80" height="80"/></a>
+            <a href="../../main.php"><img src="../img/icon-exit.png" width="80" height="80" /></a>
         </div>
     </header>
-    <div class="bar"></div>
-    <main>
 
+    <div class="bar"></div>
+
+    <main>
+        <div class="bar-left"></div>
+        <div class="container-center">
+            <div class="container-line">
+                <a href="../Funcionario/listagemFuncionario.php"><div class="widget">
+                    <img src="../img/icon-funcionario.png" width="170" height="150" />
+                    <p class="tag">Funcionários</p>
+                </div></a>
+                <a href="../Cliente/listagemCliente.php"><div class="widget">
+                    <img src="../img/icon-cliente.png" width="170" height="150" />
+                    <p class="tag">Clientes</p>
+                </div></a>
+            </div>
+            <div class="container-line">
+                <a href="../Veiculo/listagemVeiculo.php"><div class="widget">
+                    <img src="../img/icon-veiculo.png" width="170" height="150" />
+                    <p class="tag">Veículos</p>
+                </div></a>
+                <a href="../Pagamento/listagemPagamento.php"><div class="widget">
+                    <img src="../img/icon-pagamento.png" width="170" height="150" />
+                    <p class="tag">Pagamentos</p>
+                </div></a>
+                <a href="../Venda/listagemVenda.php"><div class="widget">
+                    <img src="../img/icon-venda.png" width="170" height="150" />
+                    <p class="tag">Vendas</p>
+                </div></a>
+            </div>
+        </div>
     </main>
+
+    <footer>
+        <div class="footer-penult">
+            <img class="logo" src="../img/logo.png" />
+            <div class="social">
+                <p class="pub">Siga WEBCars nas redes sociais:</p>
+                <img class="icon-social" src="../img/icon-facebook.png" />
+                <img class="icon-social" src="../img/icon-instagram.png" />
+            </div>
+        </div>
+        <div class="footer-end">
+            <p class="cookies">Copyright © WEBCars</p>
+            <p class="cookies">Política de privacidade</p>
+            <p class="cookies">Termos de uso</p>
+        </div>
+    </footer>
 
 </body>
 
